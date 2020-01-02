a536b264fd801e592b696ba9ec4547d9e91b7e60
Mohamed Abuelgheit <mabuelgheit@cloudinn.net>
2020-01-02 12:54:50 +0200

    make a small change to test the CI

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('anime-list app is running!');
+    expect(page.getTitleText()).toEqual('anime-list app is running now!');
   });
 
   afterEach(async () => {
